65ce38d68e757df72e8d447c3c7441ed12502b51
sAmH365 <npower5377@naver.com>
2024-07-20 11:22:43 +0900

    영화정보, modal, navbar

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,122 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <NavBar />
+  <Event :text="text"/>
+<h1>영화정보</h1>
+<div v-for="(movie, i ) in data" :key="i" class="item">
+  <figure>
+    <img :src="`${movie.imgUrl}`" :alt="movie.title">
+  </figure>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="info">
+    <h3 class="bg-yellow">{{ movie.title }}</h3>
+    <p>{{ movie.year }}</p>
+    <p>{{ movie.category }}</p>
+    <button @click="increaseLike(i)">좋아요</button> <span> {{ movie.like }}</span>
+    <p>
+      <button @click="isModal=true; selectedMovie=i">상세정보</button>
+    </p>
+  </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Modal />
+</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+import { data } from './assets/movies'
+import NavBar from "@/components/NavBar.vue";
+import Modal from "@/components/Modal.vue";
+import Event from "@/components/Event.vue";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isModal: false,
+      data,
+      selectedMovie: 0,
+      text: 'NETPLIX 강렬한 운명의 드라마, 경기크리처'
+    }
+  },
+  methods: {
+    increaseLike(index) {
+      this.data[index].like += 1;
+    }
+  },
+  components: {
+    NavBar,
+    Modal,
+    Event
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  max-width: 768px;
+  margin: 0 auto;
 }
 
-nav {
+h1,
+h2,
+h3 {
+  margin-bottom: 1rem;
+}
+
+p {
+  margin-bottom: 0.5rem;
+}
+
+button {
+  margin-right: 10px;
+  margin-top: 1rem;
+}
+
+.item {
   width: 100%;
-  font-size: 12px;
+  border: 1px solid #ccc;
+  display: flex;
+  margin-bottom: 20px;
+  padding: 1rem;
+}
+
+.item figure {
+  width: 30%;
+  margin-right: 1rem;
+}
+
+.item img {
+  width: 100%;
+}
+
+.item .info {
+  width: 100%;
+}
+
+.modal {
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal .inner {
+  background: #fff;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+p:has(.btn-all) {
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
